1464626c79538c6396640d1a678f89205a3a8ca7
pseudobabble <harryjohnson500@gmail.com>
2020-08-20 00:28:02 +0100

    added exhibitions

--- home/models.py
+++ home/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.db.models import TextField
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, InlinePanel
 
-from wagtail.core.models import Page
-from wagtail.core.fields import StreamField
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import StreamField, RichTextField
 from wagtail.core.blocks import CharBlock, RichTextBlock
 
 from wagtail.images.blocks import ImageChooserBlock
-
-
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
@@ -32,21 +32,70 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('hero_text'),
-        FieldPanel('hero_background'),
+        ImageChooserPanel('hero_background'),
         StreamFieldPanel('page_stream'),
     ]
 
-    # TODO  19/08/2020 14:58: Implement these properly
     parent_page_types = []
-    subpage_types = []
+    subpage_types = [
+        'home.ExhibitionIndex'
+    ]
 
 
 class ExhibitionIndex(Page):
-    pass
+    class Meta:
+        verbose_name = 'exhibitions'
+
+    page_title = TextField(null=True, blank=True)
+    blurb = RichTextField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('page_title'),
+        FieldPanel('blurb'),
+    ]
+
+    parent_page_types = ['home.HomePage']
+    subpage_types = [
+        'home.Exhibition'
+    ]
 
 class Exhibition(Page):
-    pass
+    page_title = TextField(null=True, blank=True)
+    strapline =  TextField(null=True, blank=True)
+    flyer_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    page_stream = StreamField([
+        ('heading', CharBlock()),
+        ('paragraph', RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], null=True, blank=True)
 
+    content_panels = Page.content_panels + [
+        FieldPanel('page_title'),
+        FieldPanel('strapline'),
+        ImageChooserPanel('flyer_image'),
+        StreamFieldPanel('page_stream'),
+        InlinePanel('exhibition_images')
+    ]
+
+
+class ExhibitionImage(Orderable):
+    page = ParentalKey(Exhibition, related_name='exhibition_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL, related_name='+',
+        null=True,
+        blank=True
+    )
+
+    panels = [
+        ImageChooserPanel('image')
+    ]
 
 class WorkshopIndex(Page):
     pass
