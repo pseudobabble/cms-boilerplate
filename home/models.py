4646140a75ff2544a01f07066f4389dbbb3f7b86
pseudobabble <harryjohnson500@gmail.com>
2020-08-20 13:06:00 +0100

    added workshops, artists

--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import TextField
+from django.db.models import TextField, URLField
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, InlinePanel
 
@@ -38,7 +38,9 @@ class HomePage(Page):
 
     parent_page_types = []
     subpage_types = [
-        'home.ExhibitionIndex'
+        'home.ExhibitionIndex',
+        'home.WorkshopIndex',
+        'home.ArtistIndex'
     ]
 
 
@@ -59,9 +61,10 @@ class ExhibitionIndex(Page):
         'home.Exhibition'
     ]
 
+
 class Exhibition(Page):
     page_title = TextField(null=True, blank=True)
-    strapline =  TextField(null=True, blank=True)
+    strapline = TextField(null=True, blank=True)
     flyer_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -80,8 +83,11 @@ class Exhibition(Page):
         FieldPanel('strapline'),
         ImageChooserPanel('flyer_image'),
         StreamFieldPanel('page_stream'),
-        InlinePanel('exhibition_images')
+        InlinePanel('exhibition_images', label='Exhibition Images')
     ]
+
+    parent_page_types = ['home.ExhibitionIndex']
+    subpage_types = []
 
 
 class ExhibitionImage(Orderable):
@@ -97,18 +103,111 @@ class ExhibitionImage(Orderable):
         ImageChooserPanel('image')
     ]
 
+
 class WorkshopIndex(Page):
-    pass
+    class Meta:
+        verbose_name = 'workshops'
+
+    page_title = TextField(null=True, blank=True)
+    blurb = RichTextField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('page_title'),
+        FieldPanel('blurb'),
+    ]
+
+    parent_page_types = ['home.HomePage']
+    subpage_types = [
+        'home.Workshop'
+    ]
+
 
 class Workshop(Page):
-    pass
+    page_title = TextField(null=True, blank=True)
+    strapline = TextField(null=True, blank=True)
+    page_stream = StreamField([
+        ('heading', CharBlock()),
+        ('paragraph', RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('page_title'),
+        FieldPanel('strapline'),
+        StreamFieldPanel('page_stream'),
+        InlinePanel('workshop_images', label='Workshop Images')
+    ]
+
+    parent_page_types = ['home.WorkshopIndex']
+    subpage_types = []
+
+
+class WorkshopImage(Orderable):
+    page = ParentalKey(Workshop, related_name='workshop_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL, related_name='+',
+        null=True,
+        blank=True
+    )
+
+    panels = [
+        ImageChooserPanel('image')
+    ]
 
 
 class ArtistIndex(Page):
-    pass
+    class Meta:
+        verbose_name = 'artists'
+
+    page_title = TextField(null=True, blank=True)
+    blurb = RichTextField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('page_title'),
+        FieldPanel('blurb'),
+    ]
+
+    parent_page_types = ['home.HomePage']
+    subpage_types = [
+        'home.Artist'
+    ]
 
 class Artist(Page):
-    pass
+    name = TextField(null=True, blank=True)
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL, related_name='+',
+        null=True,
+        blank=True
+    )
+    blurb = RichTextField(null=True, blank=True)
+    portfolio_link = URLField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        ImageChooserPanel('photo'),
+        FieldPanel('blurb'),
+        FieldPanel('portfolio_link'),
+        InlinePanel('portfolio_pieces', label='Portfolio Pieces')
+    ]
+
+    parent_page_types = ['home.ArtistIndex']
+    subpage_types = []
+
+
+class ArtistImage(Orderable):
+    page = ParentalKey(Artist, related_name='portfolio_pieces')
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL, related_name='+',
+        null=True,
+        blank=True
+    )
+
+    panels = [
+        ImageChooserPanel('image')
+    ]
 
 
 class Mission(Page):
